0b6b7e7b7ca7b9657db91f611d398424c01146ee
Mykytyi <40006074+Mykytyi@users.noreply.github.com>
2019-06-26 00:32:46 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,17 @@
 'use strict';
 
 function daysInMonth(month, year) {
-  try {
-    if(month > 11
-    || month < 0
-    || !Number.isInteger(month)
-    || !Number.isInteger(year)
-    || typeof year !== `number`
-    || typeof month !== `number`
-    || month === 0){
-    throw new Error('exception');
+    if (month > 11
+     || month < 0
+     || !Number.isInteger(month)
+     || !Number.isInteger(year)
+     || typeof year !== `number`
+     || typeof month !== `number`
+     || month === 0) {
+     throw new Error('exception');
   }
     const days = new Date(`${year}`, `${month}`, `0`).getDate();
     return days;
-  }
-  catch(error) {
-    console.log(error);
-  }
 }
 
 daysInMonth(5, 2019); // 31
